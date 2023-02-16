6a759fa406fa702e6f05baa76e85b643b36de0ac
Urmaturi <urmasuper93@gmail.com>
2023-02-16 19:32:08 +0600

    new

---
+++ app/src/main/java/com/example/recycleview2/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.recycleview2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+
+        recyclerView.adapter = CustomRecyclerAdapter(getCatList())
+
+    }
+    private fun fillList(): List<String> {
+        val data = mutableListOf<String>()
+        (0..30).forEach { i -> data.add("$i element") }
+        return data
+    }
+
+    private fun getCatList(): List<String> {
+        return this.resources.getStringArray(R.array.cat_names).toList()
+    }
+}
